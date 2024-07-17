0611022726ab0c7120326f4b0d5c2efebfe52120
kitoxis <exaxtsk@gmail.com>
2024-07-17 14:30:28 +0300

    Started working on database

--- src/main/java/net/starfal/Database/Database.java
+++ src/main/java/net/starfal/Database/Database.java
@@ -1,4 +1,74 @@
 package net.starfal.Database;
 
+import net.starfal.SFRanks;
+import org.bukkit.Bukkit;
+
+import java.sql.*;
+
 public class Database {
+
+    private final Connection connection;
+
+    public Database(String path) throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:" + path);
+
+        try(Statement statement = connection.createStatement()){
+            statement.execute("""
+                CREATE TABLE IF NOT EXISTS groups (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    prefix TEXT,
+                    suffix TEXT,
+                    isDefault BOOLEAN NOT NULL,
+                    permissions LIST
+                )
+                """);
+            statement.execute("""
+                CREATE TABLE IF NOT EXISTS users (
+                    nickname TEXT NOT NULL,
+                    groupIds LIST NOT NULL,
+                    prefix TEXT,
+                    suffix TEXT,
+                )
+                """);
+            statement.execute("""
+                CREATE TABLE IF NOT EXISTS group_paths (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    groupIds LIST NOT NULL
+                )
+                """);
+        }
+    }
+
+    public void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()){
+            connection.close();
+        }
+    }
+
+    public boolean userExists(String nickname) throws SQLException {
+        try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE nickname = ?")){
+            statement.setString(1, nickname);
+            try(ResultSet resultSet = statement.executeQuery()){
+                return resultSet.next();
+            }
+        }
+    }
+    public boolean groupExists(String name) throws SQLException {
+        try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM groups WHERE name = ?")){
+            statement.setString(1, name);
+            try(ResultSet resultSet = statement.executeQuery()){
+                return resultSet.next();
+            }
+        }
+    }
+    public boolean groupPathExists(String name) throws SQLException {
+        try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM group_paths WHERE name = ?")){
+            statement.setString(1, name);
+            try(ResultSet resultSet = statement.executeQuery()){
+                return resultSet.next();
+            }
+        }
+    }
 }
--- src/main/java/net/starfal/SFRanks.java
+++ src/main/java/net/starfal/SFRanks.java
@@ -1,20 +1,55 @@
 package net.starfal;
 
+import net.starfal.Database.Database;
 import net.starfal.Utilities.Color;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SFRanks extends JavaPlugin {
+    private String pluginPrefix = "<gradient:#85edff:#00ffb7>SFRanks</gradient>";
+    public String getPluginPrefix() {
+        return pluginPrefix;
+    }
+
+    private static SFRanks instance;
+    public static SFRanks getInstance() {
+        return instance;
+    }
+
+    private Database database;
 
     @Override
     public void onEnable() {
-        log("<gold>SFRanks <gray>| Enabled!");
-
+        instance = this;
+        log(pluginPrefix + " <dark_gray>| <yellow>Enabling...");
+        log(pluginPrefix + " <dark_gray>| <yellow>Trying to connect to database... <gray>(SQLite)");
+        try{
+            if (!getDataFolder().exists()){
+                getDataFolder().mkdirs();
+            }
+            database = new Database(getDataFolder().getAbsolutePath() + "/data.db");
+            log(pluginPrefix + " <dark_gray>| <green>Database Connection was successful!");
+        } catch (Exception e){
+            log(pluginPrefix + " <dark_gray>| <red>Failed to close the connection to the database! " + e.getMessage());
+            e.printStackTrace();
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+        log(pluginPrefix + " <dark_gray>| <green>Enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        log(pluginPrefix + " <dark_gray>| <yellow>Disabling...");
+        log(pluginPrefix + " <dark_gray>| <yellow>Trying to close the database connection...");
+        try{
+            database.closeConnection();
+            log(pluginPrefix + " <dark_gray>| <green>Database Connection was closed successfully!");
+        } catch (Exception e){
+            log(pluginPrefix + " <dark_gray>| <red>Failed to close the connection to the database! " + e.getMessage());
+            e.printStackTrace();
+        }
+        log(pluginPrefix + " <dark_gray>| <red>Disabled!");
     }
     public void log(String message) {
         Bukkit.getConsoleSender().sendMessage(Color.format(message));
